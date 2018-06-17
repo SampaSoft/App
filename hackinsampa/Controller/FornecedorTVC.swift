ed955ee0542a5a2952eb8a64e4db92a2261cd2cb
Carlos Doki <carlosdoki@Carloss-MacBook-Pro.local>
2018-06-17 06:36:26 -0300

    Funcionalidades

---
+++ hackinsampa/Controller/FornecedorTVC.swift
@@ -0,0 +1,24 @@
+//
+//  FornecedorTVC.swift
+//  hackinsampa
+//
+//  Created by Carlos Doki on 17/06/18.
+//  Copyright © 2018 Carlos Doki. All rights reserved.
+//
+
+import UIKit
+
+class FornecedorTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ hackinsampa/Datasource/Fornecedor.swift
@@ -0,0 +1,9 @@
+//
+//  Fornecedor.swift
+//  hackinsampa
+//
+//  Created by Carlos Doki on 17/06/18.
+//  Copyright © 2018 Carlos Doki. All rights reserved.
+//
+
+import Foundation
